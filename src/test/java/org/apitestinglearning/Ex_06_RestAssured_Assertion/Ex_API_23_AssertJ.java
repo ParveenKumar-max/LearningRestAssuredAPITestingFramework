429284d9111f6ab0278624b094fc54c629ba8892
ParveenKumar-max <Parveendogra2@gmail.com>
2025-07-11 00:20:10 +0530

    RestAssured - AssertJ with TestNG assertion

--- src/test/java/org/apitestinglearning/Ex_06_RestAssured_Assertion/Ex_API_23_AssertJ.java
+++ src/test/java/org/apitestinglearning/Ex_06_RestAssured_Assertion/Ex_API_23_AssertJ.java
@@ -9,6 +9,7 @@ import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Matchers;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -18,8 +19,8 @@ public class Ex_API_23_AssertJ {
     RequestSpecification RS;
     Response R;
     ValidatableResponse VR;
-   /* String token;
-    Integer bookingID;      As of now, there is no use */
+    /* String token; As of now, there is no use */
+    Integer bookingID;
 
 
     @Owner("Parveen")
@@ -57,10 +58,38 @@ public class Ex_API_23_AssertJ {
 
         // firstname == Parveen , Last Name = Chaudhary
         // Booking ID shouldn't null
+        // Rest Assured -> import org.hamcrest.Matchers; %4-%5
+        // Matchers.equalto()
 
         VR.body("booking.firstname", Matchers.equalTo("Parveen"));
         VR.body("booking.lastname", Matchers.equalTo("Chaudhary"));
         VR.body("bookingid", Matchers.notNullValue());
+
+        // TestNG - Extract the details of the firstname, bookingId, lastname from Response.
+
+        bookingID = R.then().extract().path("bookingid");
+        String Firstname = R.then().extract().path("booking.firstname");
+        String Lastname = R.then().extract().path("booking.lastname");
+
+
+        // TestNG Assertions - 75%
+        // SoftAssert vs
+        // HardAssert -
+        // This means that if any assertion fails,
+        // the remaining statements in that test method will not be executed.
+
+        Assert.assertEquals(Firstname, "Parveen");
+        Assert.assertEquals(Lastname, "Chaudhary");
+        Assert.assertNull(bookingID);
+
+
+        // AssertJ( 3rd- Lib to Assertions) - 20%
+        assertThat(bookingID).isNotNull().isNotNegative().isNotZero();
+        assertThat(Firstname).isEqualTo("Parveen").isNotEmpty().isNotBlank();
+        assertThat(Lastname).isEqualTo("Chaudhary").isNotEmpty().isNotBlank();
+
+        //        String s = ""; //Empty
+        //        String s2 = " "; //Blank
 
 
     }
